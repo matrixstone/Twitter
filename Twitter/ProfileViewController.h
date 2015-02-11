83a1770ba76f245761a31d5b77b27b2dfaabe93f
xuhe <xuhe@yahoo-inc.com>
2015-02-10 23:25:15 -0800

    try to add hanburger menu

---
+++ Twitter/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Twitter
+//
+//  Created by Xu He on 2/10/15.
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
